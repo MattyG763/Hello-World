ba02fc80639634b11dbe344feb69bdcf32607ad3
Mathew Greenwood <56030783+MattyG763@users.noreply.github.com>
2023-10-02 11:24:52 -0400

    Changed to input to make the name title case
    Changed the input to make title case

--- Hello World/main.py
+++ Hello World/main.py
@@ -1,13 +1,12 @@
 # System outputs hello world to the console
 
-
-# Ask the user their name
+# Ask the user for name
 def name():
-    return input("What is your name? ")
+    return input("What is your name? ").title()
 
 # Print name to the console
 def print_name(name):
-    print(f"Hello, {name}")
+    print(f"Hello, {name}!")
 
 def main():
     print_name(name())
